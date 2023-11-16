2defc87a07a92db5987db57ffd6ab5dd8fe2d466
ToporekDaniel <danieltoporek0@gmail.com>
2023-11-17 00:11:02 +0100

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bg = document.querySelector("body");
+const color = document.querySelector("span.color");
+const button = document.querySelector("button.change-color");
+
+button.addEventListener("click", () => {
+  bg.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+});
