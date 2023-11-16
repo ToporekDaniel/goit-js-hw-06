5c1be5703babaf080d37ce61e460355dc65f70b5
ToporekDaniel <danieltoporek0@gmail.com>
2023-11-16 23:57:11 +0100

    task-08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector("form.login-form");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Wypełnij wszystkie pola formularza.");
+    return;
+  }
+  console.log(email.value, password.value);
+  event.target.reset();
+});
